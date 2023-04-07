118869d628ee293b6cdab30333d7ff3f21a3f5b1
mtfcd <mtfcd_cc@126.com>
2023-04-07 18:30:02 +0800

    system tray menu logic

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -8,8 +8,7 @@ extern crate log;
 #[macro_use]
 extern crate lazy_static;
 
-use std::sync::{Arc, Mutex};
-use tauri::{AppHandle, GlobalShortcutManager, Manager, Window};
+use tauri::{AppHandle, GlobalShortcutManager, Manager};
 use tauri::{CustomMenuItem, SystemTray, SystemTrayEvent, SystemTrayMenu};
 use tauri_plugin_log::LogTarget;
 use utils::word;
@@ -25,7 +24,7 @@ async fn lookup(word: String) -> Result<String, String> {
     }
 }
 
-fn handle_short_cut(card_win: Arc<Mutex<Window>>) {
+fn handle_short_cut(app: AppHandle) {
     tauri::async_runtime::spawn(async move {
         let def_res = logic::get_def().await;
         if def_res.is_none() {
@@ -34,13 +33,23 @@ fn handle_short_cut(card_win: Arc<Mutex<Window>>) {
         }
 
         let def = def_res.unwrap();
-        let win = card_win.lock().unwrap();
-        if !win.is_visible().unwrap() {
-            win.show().unwrap();
-        }
+        lookup_window_visibility(&app);
+        let win = app.get_window("lookup").unwrap();
         win.emit("showDef", def)
             .unwrap_or_else(|e| println!("emmit error {}", e));
     });
+}
+
+fn lookup_window_visibility(app: &AppHandle) {
+    let item_handle = app.tray_handle().get_item("hide");
+    let lookup_window = app.get_window("lookup").unwrap();
+    if lookup_window.is_visible().unwrap() {
+        lookup_window.hide().unwrap();
+        item_handle.set_title("Show Lookup").unwrap();
+    } else {
+        lookup_window.show().unwrap();
+        item_handle.set_title("Hide Lookup").unwrap();
+    }
 }
 
 fn handle_tray_event(app: &AppHandle, event: SystemTrayEvent) {
@@ -50,12 +59,12 @@ fn handle_tray_event(app: &AppHandle, event: SystemTrayEvent) {
                 std::process::exit(0);
             }
             "hide" => {
-                app.get_window("lookup").unwrap().hide().unwrap();
+                lookup_window_visibility(app);
             }
             _ => {}
         },
         SystemTrayEvent::DoubleClick { .. } => {
-            app.get_window("lookup").unwrap().show().unwrap();
+            lookup_window_visibility(app);
         }
         _ => {}
     }
@@ -63,7 +72,7 @@ fn handle_tray_event(app: &AppHandle, event: SystemTrayEvent) {
 
 fn build_tray() -> SystemTray {
     let quit = CustomMenuItem::new("quit".to_string(), "Quit");
-    let hide = CustomMenuItem::new("hide".to_string(), "Hide Window");
+    let hide = CustomMenuItem::new("hide".to_string(), "Hide Lookup");
     let tray_menu = SystemTrayMenu::new().add_item(hide).add_item(quit);
     SystemTray::new().with_menu(tray_menu)
 }
@@ -77,11 +86,12 @@ fn main() {
                 .build(),
         )
         .setup(move |app| {
-            let card_win = app.get_window("lookup").unwrap();
-            let ref_win = Arc::new(Mutex::new(card_win));
+            let app_handle = app.app_handle();
             let mut gs_manager = app.global_shortcut_manager();
             gs_manager
-                .register("Ctrl+Shift+c", move || handle_short_cut(ref_win.clone()))
+                .register("Ctrl+Shift+c", move || {
+                    handle_short_cut(app_handle.clone());
+                })
                 .unwrap();
             Ok(())
         })
@@ -89,8 +99,11 @@ fn main() {
         .on_system_tray_event(handle_tray_event)
         .on_window_event(|event| {
             if let tauri::WindowEvent::CloseRequested { api, .. } = event.event() {
-                event.window().hide().unwrap();
+                let win = event.window();
+                win.hide().unwrap();
                 api.prevent_close();
+                let item_handle = win.app_handle().tray_handle().get_item("hide");
+                item_handle.set_title("Show Lookup").unwrap();
             }
         })
         .invoke_handler(tauri::generate_handler![lookup])
